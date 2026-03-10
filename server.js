a1068e68cbd4dbba2845a360070745f0be7c0d91
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-03-10 15:19:39 -0300

    feat: serve HTML files using fs

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 import http from 'http';
+import fs from 'fs/promises';
 
 http.createServer((req, res) => {
   res.end('Hello world');
+
+  serveFile(res, fileName = './index.html');
+  
 }).listen(8080);
+
+const serveFile = async (res, fileName) => {
+    try {
+        const data = await fs.readFile(fileName, 'utf8');
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        res.write(data);
+
+        res.end();
+    }
+    catch (err){
+        console.error(err)
+
+        res.writeHead(500);
+
+        res.end('Internal Server Error');
+    }
+}
